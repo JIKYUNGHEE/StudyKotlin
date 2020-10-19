c936f5cdd7f62c1b6c7a009cb7dae73f69d334db
khji <khji@mobileleader.com>
2020-10-19 18:12:10 +0900

    SplashActivity  생성

---
+++ app/src/main/java/com/gaeng0517/kotlinpractice/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.gaeng0517.kotlinpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh)
+    }
+
+}
