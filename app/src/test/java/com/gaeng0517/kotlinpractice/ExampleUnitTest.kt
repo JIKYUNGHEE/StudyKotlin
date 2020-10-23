687d3436eb6317aacfad7b937966906b3ec43b7d
khji <khji@mobileleader.com>
2020-10-23 15:38:29 +0900

    Kotlin Practice2 - list

--- app/src/test/java/com/gaeng0517/kotlinpractice/ExampleUnitTest.kt
+++ app/src/test/java/com/gaeng0517/kotlinpractice/ExampleUnitTest.kt
@@ -320,4 +320,35 @@ class ExampleUnitTest {
         println(hasVisitedSpain)
         println(hasNotVisitedSpain)
     }
+
+    @Test
+    fun test2_3() {
+        val countries = listOf("Russia", "USA", "The Netherlands")
+        println(countries)
+
+//        val mutableList = mutableListOf<>("Russia", "USA", "The Netherlands")
+        val mutableList = countries.toMutableList()
+        mutableList.add("Germany")
+        mutableList.add(1, "Greece")
+
+        println(mutableList)
+        mutableList.addAll(3, listOf("Austria", "Poland", "Hungary"))
+        println(mutableList)
+
+        val hasBeenToJapan = "Japan" in mutableList
+        println(hasBeenToJapan)
+
+        mutableList.remove("Japan")
+        mutableList.removeAt(0)
+        mutableList.removeAll(listOf("Greece", "Poland", "Hungary"))
+        println(mutableList)
+
+        mutableList[2] = "Croatia"
+        val combinedList = countries + mutableList
+        val emptyList = emptyList<String>()
+
+        println(mutableList)
+        println(combinedList)
+        println(emptyList)
+    }
 }
