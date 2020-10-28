32955b7689b5773bae00583580ba3cd7984ecb9b
JIKYUNGHEE <wlrudgml517@gmail.com>
2020-10-28 11:53:15 +0900

    test 커밋

---
+++ app/src/main/java/com/gaeng0517/kotlinpractice/own/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gaeng0517.kotlinpractice.own
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gaeng0517.kotlinpractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_own_main)
+    }
+}
