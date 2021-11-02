867fdd8a1cc461a56b61e9dfff7cac4dd76cc77e
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:16:18 +0900

    - 3.14 확장함수로

--- app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
@@ -2,18 +2,22 @@ package com.gaeng0517.kotlininaction.ch3
 
 class User(val id: Int, val name: String, val address: String)
 
-fun saveUser(user: User) {
+fun User.validateBeforeSave() {
     fun validate(
         value: String,
         fieldName: String
     ) {
         if (value.isEmpty()) {
-            throw IllegalArgumentException("Can't save user ${user.id} : $fieldName")
+            throw IllegalArgumentException("Can't save user $id : $fieldName")
         }
     }
 
-    validate(user.name, "Name")
-    validate(user.address, "Address")
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+fun saveUser(user: User) {
+    user.validateBeforeSave()
 
     //TODO. user 를 db 에 저장한다.
 }
