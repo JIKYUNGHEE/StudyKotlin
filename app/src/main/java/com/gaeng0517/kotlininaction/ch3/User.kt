b479d0a0423ccdf8069784c9d0dcc2e703055cbe
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:14:07 +0900

    - 3.13 로컬 함수에서 바깥 함수의 파라미터 접근

--- app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
@@ -1,19 +1,19 @@
 package com.gaeng0517.kotlininaction.ch3
 
-class User(val id:Int, val name: String, val address: String)
+class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
-    fun validate(user: User,
-                 value: String,
-                 fieldName: String) {
-        if(value.isEmpty()) {
+    fun validate(
+        value: String,
+        fieldName: String
+    ) {
+        if (value.isEmpty()) {
             throw IllegalArgumentException("Can't save user ${user.id} : $fieldName")
         }
     }
 
-    //FIXME. user 객체를 로컬함수에 계속 전달해야 함!
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
+    validate(user.name, "Name")
+    validate(user.address, "Address")
 
     //TODO. user 를 db 에 저장한다.
 }
