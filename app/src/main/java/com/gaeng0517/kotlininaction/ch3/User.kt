22afbe5752b3b90d51bb090a49d4e7b6a345fbf4
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:11:22 +0900

    - 3.12 로컬 함수 사용 > 코드 중복 줄이기

--- app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
@@ -3,14 +3,16 @@ package com.gaeng0517.kotlininaction.ch3
 class User(val id:Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
-    //검증 코드의 중복
-    if(user.name.isEmpty()) {
-        throw IllegalArgumentException("Can't save user ${user.id} : empty Name")
+    fun validate(user: User,
+                 value: String,
+                 fieldName: String) {
+        if(value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${user.id} : $fieldName")
+        }
     }
 
-    if(user.address.isEmpty()) {
-        throw IllegalArgumentException("Can't save user ${user.id} : empty Address")
-    }
+    validate(user, user.name, "Name")
+    validate(user, user.address, "Address")
 
     //TODO. user 를 db 에 저장한다.
 }
