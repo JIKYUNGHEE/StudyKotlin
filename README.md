c1c7065e4f7b2ff2e5cebad0489c895b8aed419c
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:08:02 +0900

    - Kotlin in Action 예제 추가 - 리스트 3.11 필드 검증 중복 예제

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # KotlinPractice
 
 Kotlin 연습용 프로젝트
+
+Kotlin in Action 예제 추가
---
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
@@ -0,0 +1,16 @@
+package com.gaeng0517.kotlininaction.ch3
+
+class User(val id:Int, val name: String, val address: String)
+
+fun saveUser(user: User) {
+    //검증 코드의 중복
+    if(user.name.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${user.id} : empty Name")
+    }
+
+    if(user.address.isEmpty()) {
+        throw IllegalArgumentException("Can't save user ${user.address} : empty Address")
+    }
+
+    //TODO. user 를 db 에 저장한다.
+}
