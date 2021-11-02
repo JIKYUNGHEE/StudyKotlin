ea0ff93eabf0b1943be2bf67aaa58aaf68a3f9cb
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:43:51 +0900

    - 리스트 3.10 경로 파싱에 정규식 사용하기(3중 따옴표 문자열)

--- app/src/main/java/com/gaeng0517/kotlininaction/ch3/5/String.kt
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/5/String.kt
@@ -1,10 +1,19 @@
 package com.gaeng0517.kotlininaction.ch3.`5`
 
-fun parsePath(path: String) {
-    val directory = path.substringBeforeLast("/")
-    val fullName = path.substringAfterLast("/")
-    val fileName = fullName.substringBeforeLast(".")
-    val extension = fullName.substringAfterLast(".")
+//fun parsePath(path: String) {
+//    val directory = path.substringBeforeLast("/")
+//    val fullName = path.substringAfterLast("/")
+//    val fileName = fullName.substringBeforeLast(".")
+//    val extension = fullName.substringAfterLast(".")
+//
+//    println("dir: $directory, name: $fileName, ext: $extension")
+//}
 
-    println("dir: $directory, name: $fileName, ext: $extension")
+fun parsePath(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if(matchResult != null) {
+        val (directory, fileName, extension) = matchResult.destructured
+        println("dir: $directory, name: $fileName, ext: $extension")
+    }
 }
