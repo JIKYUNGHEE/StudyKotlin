8e2c0219d230654e7d35d1f767aa0cb34950c8f4
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-11-03 00:20:39 +0900

    - (+)확장함수를 로컬함수로 정의 (-) 중첩된 함수의 깊이가 깊으면, 코드를 읽기 어렵다.

--- app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
+++ app/src/main/java/com/gaeng0517/kotlininaction/ch3/User.kt
@@ -2,21 +2,21 @@ package com.gaeng0517.kotlininaction.ch3
 
 class User(val id: Int, val name: String, val address: String)
 
-fun User.validateBeforeSave() {
-    fun validate(
-        value: String,
-        fieldName: String
-    ) {
-        if (value.isEmpty()) {
-            throw IllegalArgumentException("Can't save user $id : $fieldName")
+fun saveUser(user: User) {
+    fun User.validateBeforeSave() {
+        fun validate(
+            value: String,
+            fieldName: String
+        ) {
+            if (value.isEmpty()) {
+                throw IllegalArgumentException("Can't save user $id : $fieldName")
+            }
         }
+
+        validate(name, "Name")
+        validate(address, "Address")
     }
 
-    validate(name, "Name")
-    validate(address, "Address")
-}
-
-fun saveUser(user: User) {
     user.validateBeforeSave()
 
     //TODO. user 를 db 에 저장한다.
